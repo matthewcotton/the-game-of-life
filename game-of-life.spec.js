f6d92ce4219512b10f13741fac455ea12cf66471
matthewcotton <matthewneilcotton@gmail.com>
2020-11-20 10:57:20 -0500

    remove complex test & add simple test

--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -1,18 +1,6 @@
 import { gameOfLife } from "./game-of-life"
 
-describe("Game Of Life", function(){
-    it("#1: any live cell with fewer than two live neighbours dies, as if caused by under-population", function(){
-        var liveCell = world.getCell(0, 0);
-        liveCell.live();
-        world.tick();
-        expect(liveCell.isDead()).toBeTruthy();
-  
-        var liveCellWithOneNeighbour = world.getCell(0, 0);
-        var cellAtRight = world.getCell(0, 1);
-        liveCellWithOneNeighbour.live();
-        cellAtRight.live();
-        world.tick();
-        expect(liveCellWithOneNeighbour.isDead()).toBeTruthy();
-        expect(cellAtRight.isDead()).toBeTruthy();
-      })
-    });
+describe("live cell has 1 live neighbours, dies", () => {
+  const result = gameOfLife(1);
+  expect(result).toBe(false);
+});
