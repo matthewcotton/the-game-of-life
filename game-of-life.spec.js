b6880b5cefda1d3d6e2ee93cf795208bc052cb69
matthewcotton <matthewneilcotton@gmail.com>
2020-11-24 11:29:51 -0500

    added 1st dead cell test

--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -2,22 +2,27 @@ import { gameOfLife } from "./game-of-life"
 
 describe ("Game of Life Tests:", () => {
 it("live cell has 1 live neighbours, dies", () => {
-  const result = gameOfLife("1");
+  const result = gameOfLife("1", "alive");
   expect(result).toBe(false);
 })
 
-it("live cell with fewer than 2 live neighbours, dies", () => {
-  const result = gameOfLife("2");
+it("live cell has 3 live neighbours, lives", () => {
+  const result = gameOfLife("2", "alive");
   expect(result).toBe(true);
 })
 
 it("live cell has more than 3 live neighbours, dies", () => {
-  const result = gameOfLife("4");
+  const result = gameOfLife("4", "alive");
   expect(result).toBe(false);
 })
 
-it("live cell has 2 or 3 live neighbours, lives", () => {
-  const result = gameOfLife("3");
+it("live cell has 3 live neighbours, lives", () => {
+  const result = gameOfLife("3", "alive");
   expect(result).toBe(true);
+})
+
+it("dead cell with less than 3 neighbours, stays dead", () => {
+  const result = gameOfLife("2", "dead");
+  expect(result).toBe(false);
 })
 });
