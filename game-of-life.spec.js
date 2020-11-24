aad9263783254374078c6579522e76cfa0df90a3
matthewcotton <matthewneilcotton@gmail.com>
2020-11-23 21:49:26 -0500

    add 3rd test

--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -1,14 +1,18 @@
 import { gameOfLife } from "./game-of-life"
 
-describe ("cell", () => {
+describe ("Game of Life Tests:", () => {
 it("live cell has 1 live neighbours, dies", () => {
   const result = gameOfLife("1");
   expect(result).toBe(false);
 })
 
-
 it("live cell with fewer than 2 live neighbours, dies", () => {
-  const result = gameOfLife("2")
+  const result = gameOfLife("2");
   expect(result).toBe(true);
+})
+
+it("live cell has more than 3 live neighbours, dies", () => {
+  const result = gameOfLife("4");
+  expect(result).toBe(false);
 })
 });
